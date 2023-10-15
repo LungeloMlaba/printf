9a28a5ec27b1ef25fc46775efda13305bb2647a2
LungeloMlaba <lungelomlaba49@gmail.comlungelomlaba49@gmail.com>
2023-10-15 08:40:01 -0700

    qq

--- _printf.c
+++ _printf.c
@@ -1,16 +1,15 @@
-<<<<<<< HEAD
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format of printf
- * Return: Printed chars
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int b, print = 0, print_chars = 0;
+	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -20,11 +19,11 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (b = 0; format && format[i] != '\0'; b++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[b];
+			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
@@ -33,16 +32,16 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &b);
-			width = get_width(format, &b, list);
-			precision = get_precision(format, &b, list);
-			size = get_size(format, &b);
-			++b;
-			print = handle_print(format, &b, list, buffer,
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
-			if (print == -1)
+			if (printed == -1)
 				return (-1);
-			print_chars += print;
+			printed_chars += printed;
 		}
 	}
 
@@ -50,7 +49,7 @@ int _printf(const char *format, ...)
 
 	va_end(list);
 
-	return (print_chars);
+	return (printed_chars);
 }
 
 /**
@@ -64,49 +63,4 @@ void print_buffer(char buffer[], int *buff_ind)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
-=======
-#include <unistd.h>
-#include <stdarg.h>
-#include "main.h"
-
-int _printf(const char *format, ...)
-{
-	int i, stlen;
-	va_list args;
-	char c;
-	char *s;
-	int n;
-	/*char *checker = "cs";*/
-	/*{
-		{"%s", printf_string}, {"%c", printf_char},
-	}*/
-	
-	va_start(args, format);
-	n = 0;
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			c = va_arg(args, int);
-			_putchar(c);
-			n++;
-			i+=2;
-		}
-		else if (format[i] == '%' && format[i + 1] == 's')
-		{
-			s = va_arg(args, char*);
-			for (stlen = 0; s[stlen] != '\0'; stlen++)
-			{
-				_putchar(s[stlen]);
-			}
-			i+=2;
-			n++;
-		}
-		else
-		{
-			_putchar(format[i]);
-			n++;
-		}
-	}
-	return (n);
 }
