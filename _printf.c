046a4a8fad9c1bc1d1a25001ad889b037344ad39
Pizza Man <adesekosamdev@gmail.com>
2023-10-17 16:39:28 +0000

    update tasks

--- _printf.c
+++ _printf.c
@@ -5,25 +5,21 @@
  * @format: identifier to look for.
  * Return: the length of the string.
  */
+
 int _printf(const char * const format, ...)
 {
-	checker r[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%i", printf_int}, {"%d", printf_dec},
-		{"%b", printf_binary}, {"%u", printf_unsigned_int},
-		{"%o", printf_octal}, {"%x", printf_hexad},
-		{"%X", printf_HEXAD}, {"%S", printf_extra_string},
-		{"%p", printf_pointer}, {"%R", printf_rot13},
-		{"%r", printf_reversed_string}, {"%%", printf_char37}
-	};
-
+	checker r[] = {{"%X", printf_HEXAD}, {"%c", printf_char}, {"%i", printf_int},
+		{"%d", printf_dec}, {"%R", printf_rot13}, {"%o", printf_octal},
+		{"%u", printf_unsigned_int}, {"%x", printf_hexad},
+		{"%s", printf_string}, {"%S", printf_extra_string},
+		{"%p", printf_pointer}, {"%b", printf_binary},
+		{"%r", printf_reversed_string}, {"%%", printf_char37} };
 	va_list args;
 	int i = 0, j, chars_printed = 0;
 
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
 	while (format[i] != '\0')
 	{
 		int match_found = 0;
